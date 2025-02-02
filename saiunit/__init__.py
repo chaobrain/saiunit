08dec1fa9d38e19dd558df855c0407e14fc0516d
chaoming <adaduo@outlook.com>
2025-02-02 16:30:26 +0800

    add automatic doc and wheel building

--- saiunit/__init__.py
+++ saiunit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from . import _matplotlib_compat
 from . import autograd
