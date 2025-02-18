23846c08bdd1f39c2108f9ba5138421e43f86171
Chaoming Wang <adaduo@outlook.com>
2025-02-18 13:25:57 +0800

    fix unit rdiv operation (#11)

--- saiunit/__init__.py
+++ saiunit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from . import _matplotlib_compat
 from . import autograd
