62e001490b41f32be915a9267ebff7f010b449a1
routhleck <sichaohe@stu.pku.edu.cn>
2025-04-19 10:20:57 +0800

    Add import for `_linalg_remove_unit` and update `__all__` definition

--- saiunit/linalg/__init__.py
+++ saiunit/linalg/__init__.py
@@ -17,9 +17,13 @@ from ._linalg_change_unit import *
 from ._linalg_change_unit import __all__ as _linalg_change_unit_all
 from ._linalg_keep_unit import *
 from ._linalg_keep_unit import __all__ as _linalg_keep_unit_all
+from ._linalg_remove_unit import *
+from ._linalg_remove_unit import __all__ as _linalg_remove_unit_all
 
 __all__ = (_linalg_change_unit_all +
-              _linalg_keep_unit_all)
+              _linalg_keep_unit_all +
+           _linalg_remove_unit_all)
 
 del (_linalg_change_unit_all,
-     _linalg_keep_unit_all)
+     _linalg_keep_unit_all,
+     _linalg_remove_unit_all)
