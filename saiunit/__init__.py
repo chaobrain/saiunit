1cc6e026a016056197a3d767a44892bd89366728
chaoming <adaduo@outlook.com>
2025-05-11 18:40:15 +0800

    Bump version to 0.0.12

--- saiunit/__init__.py
+++ saiunit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "0.0.11"
+__version__ = "0.0.12"
 
 from . import _matplotlib_compat
 from . import autograd
