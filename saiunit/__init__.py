d5bd447eaee2228c0512205d731b5d1fb154a1a3
chaoming <adaduo@outlook.com>
2025-06-02 11:18:00 +0800

    Bump version to 0.0.15 and update wheel upload paths in build_wheel.sh

--- saiunit/__init__.py
+++ saiunit/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 from . import _matplotlib_compat
 from . import autograd
