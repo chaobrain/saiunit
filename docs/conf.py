3f77136f96b92487edb06e5a47e59bc4f64cc1d1
Chaoming Wang <wcm.brainsim@gmail.com>
2026-05-07 10:59:50 +0800

    Self-host docs at brainx.chaobrain.com/brainunit/
    - Add brainx_sphinx_header Sphinx extension for shared header.
    - Add deploy-docs.yml: zero-downtime release-symlink deploy to
      /var/www/brainx.chaobrain.com.pkgs/brainunit/.
    - Set html_baseurl to https://brainx.chaobrain.com/brainunit/.

--- docs/conf.py
+++ docs/conf.py
@@ -81,8 +81,12 @@ extensions = [
     'myst_nb',
     'matplotlib.sphinxext.plot_directive',
     'sphinx_thebe',
-    'sphinx_design'
+    'sphinx_design',
+    'brainx_sphinx_header',
 ]
+
+html_baseurl = 'https://brainx.chaobrain.com/brainunit/'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
